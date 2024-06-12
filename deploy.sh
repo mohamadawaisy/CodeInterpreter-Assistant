4513832b9c57c78bde70c10efdff6af066a43e43
AbedAwaisy <abed.awaisy.a@gmail.com>
2024-06-12 16:31:35 +0300

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 # Navigate to the project directory
 cd /home/ubuntu/CodeInterpreter-Assistant/data_assistant_project
 
+# Export environment variables
+export OPENAI_API_KEY=${OPENAI_API_KEY}
+
 # Activate virtual environment
 source /home/ubuntu/CodeInterpreter-Assistant/venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
-
-# Export environment variables
-export OPENAI_API_KEY=${OPENAI_API_KEY}
 
 # Restart the systemd service
 sudo systemctl restart data_assistant.service
